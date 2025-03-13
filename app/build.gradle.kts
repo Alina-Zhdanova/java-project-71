70513befe96ba1f29897d5e646e5e4a4ce6e985e
Alina Zhdanova <gdanova98@gmail.com>
2025-03-13 21:36:45 +0700

    Step 4 - Arguments and Options. 1. The code of the App file.java has been modified so that when running with the -h flag, help is displayed, as specified in the task.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    implementation ("info.picocli:picocli:4.7.5")
-    annotationProcessor ("info.picocli:picocli-codegen:4.7.5")
+    implementation ("info.picocli:picocli:4.7.6")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,24 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 class App implements Runnable {
+
+    @Parameters(paramLabel = "filepath1", description = "Path to first file.")
+    File file1;
+
+    @Parameters(paramLabel = "filepath2", description = "Path to second file.")
+    File file2;
+
+    @Option(names = {"-f", "--format"}, description = "Output format [default: stylish].", paramLabel = "format")
+    private String format;
 
     @Override
     public void run() {
