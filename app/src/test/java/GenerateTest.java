53ead171fe8983cac96b91fd082b22bae91c3c36
Alina Zhdanova <gdanova98@gmail.com>
2025-03-21 02:13:00 +0700

    Step 7 - Automatic testing and CI (continuous integration).

--- app/src/test/java/GenerateTest.java
+++ app/src/test/java/GenerateTest.java
@@ -4,8 +4,16 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GenerateTest {
+
+    @Test
+    public void testNoSuchFile() {
+        var path1 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file2.json";
+        var path2 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file3.json";
+        assertThrows(IOException.class, () -> Differ.generate(path1, path2));
+    }
 
     @Test
     public void testGenerate() throws IOException {
@@ -20,8 +28,9 @@ public class GenerateTest {
                   + verbose: true
                 }""";
 
-        var expected = Differ.generate("/Users/alinazdanova/java-project-71/app/src/test/resources/file1.json",
-                                       "/Users/alinazdanova/java-project-71/app/src/test/resources/file2.json");
+        var path1 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file1.json";
+        var path2 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file2.json";
+        var expected = Differ.generate(path1, path2);
 
         assertEquals(actual, expected);
     }
