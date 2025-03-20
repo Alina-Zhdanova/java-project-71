3e85a7a54cbf0b228149df7c83f866e4fbcb5e75
Alina Zhdanova <gdanova98@gmail.com>
2025-03-21 02:50:32 +0700

    Step 7 - Automatic testing and CI (continuous integration).

--- app/src/test/java/GenerateTest.java
+++ app/src/test/java/GenerateTest.java
@@ -2,16 +2,22 @@ import hexlet.code.Differ;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GenerateTest {
 
+    private static String getPath(String fileName) {
+        return String.valueOf(Paths.get("src", "test", "resources", fileName)
+                .toAbsolutePath().normalize());
+    }
+
     @Test
     public void testNoSuchFile() {
-        var filePath1 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file2.json";
-        var filePath2 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file3.json";
+        var filePath1 = getPath("file2.json");
+        var filePath2 = getPath("file3.json");
         assertThrows(IOException.class, () -> Differ.generate(filePath1, filePath2));
     }
 
@@ -28,8 +34,8 @@ public class GenerateTest {
                   + verbose: true
                 }""";
 
-        var filePath1 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file1.json";
-        var filePath2 = "/Users/alinazdanova/java-project-71/app/src/test/resources/file2.json";
+        var filePath1 = getPath("file1.json");
+        var filePath2 = getPath("file2.json");
         var expected = Differ.generate(filePath1, filePath2);
 
         assertEquals(actual, expected);
